3f47bc790496f4bc86df349ab01599e7fcec0e53
D.M Arbaaz <arbaaz2410@gmail.com>
2017-11-09 19:09:00 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as pt
 import pandas as pd
+import time
 from sklearn.tree import DecisionTreeClassifier
 
+x=int(input("Give the index of the test image you would like to predict : "))
 #Read from train.csv
 data=pd.read_csv("train.csv").as_matrix()
 
@@ -10,17 +12,20 @@ data=pd.read_csv("train.csv").as_matrix()
 clf = DecisionTreeClassifier()
 
 #training data
-xtrain=data[16000:,1:]
-train_label=data[16000:,0]
+xtrain=data[20000:,1:]
+train_label=data[20000:,0]
 
 #testing data
 xtest=data[0:21000,1:]
 actual_label=data[0:21000:,0]
 
 #training the classifier
+print("training classifier.....")
 clf.fit(xtrain,train_label)
 
 #Calculating accuracy
+print("Predicting the image at index %d ....."%x)
+time.sleep(2)
 p=clf.predict(xtest)
 count=0
 for i in range(0,21000):
@@ -28,9 +33,9 @@ for i in range(0,21000):
         count+=1
 
 #sample test data (csv-2) , We now predict an image
-d=xtest[47]
+d=xtest[x]
 d.shape=(28,28)
 pt.imshow(255-d,cmap='gray')
-print("With an Accuracy of : ",(count/21000)*100)
-print("The Number is : ",clf.predict( [xtest[47]] ))
+print("With a Probab of : ",(count/21000)*100)
+print("The Number is :",clf.predict( [xtest[x]] ))
 pt.show()
